e163360188a9102fd123be74b0f224fb560d013f
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-02-12 06:19:05 +0200

    refactored code with resource usage

--- app/src/main/java/com/example/gitrobotdreamslessonssaveddata/MainActivity.kt
+++ app/src/main/java/com/example/gitrobotdreamslessonssaveddata/MainActivity.kt
@@ -6,47 +6,43 @@ import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private var myNumberAsString:String = ""
+
     private var myNumber:Int = 0
+    private var textHolder:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textMyNumber: TextView = findViewById(R.id.myNumber)
-        myNumberAsString = Myapplication.getApp().getSavedData()
-        if(myNumberAsString == "") myNumber = 0
-        else myNumber = myNumberAsString.toInt()
-        textMyNumber.text = "My number is: $myNumber"
         val fabButtonPlus: FloatingActionButton = findViewById(R.id.fabPlus)
         val fabButtonMinus: FloatingActionButton = findViewById(R.id.fabMinus)
 
+        myNumber = Myapplication.getApp().getSavedNumber()
+        textHolder = getString(R.string.txt_my_number)
+        textMyNumber.text = buildString {
+            append(textHolder)
+            append(myNumber)
+        }
+
         fabButtonPlus.setOnClickListener {
-            if(myNumberAsString == "") {
-                myNumber = 0
-            }
-            else {
-                myNumber = myNumberAsString.toInt()
-            }
             myNumber++
-            myNumberAsString = myNumber.toString()
-            myNumberAsString.let { Myapplication.getApp().saveData(it) }
-            textMyNumber.text = "My number is: $myNumber"
+            Myapplication.getApp().saveNumber(myNumber)
+            textMyNumber.text = buildString {
+                append(textHolder)
+                append(myNumber)
+            }
         }
 
         fabButtonMinus.setOnClickListener {
-            if(myNumberAsString == "") {
-                myNumber = 0
-            }
-            else {
-                myNumber = myNumberAsString.toInt()
-            }
             myNumber--
-            myNumberAsString = myNumber.toString()
-            myNumberAsString.let { Myapplication.getApp().saveData(it) }
-            textMyNumber.text = "My number is: $myNumber"
+            Myapplication.getApp().saveNumber(myNumber)
+            textMyNumber.text = buildString {
+                append(textHolder)
+                append(myNumber)
+            }
         }
     }
     override fun onPause() {
         super.onPause()
-        myNumberAsString.let { Myapplication.getApp().saveData(it) }
+        Myapplication.getApp().saveNumber(myNumber)
     }
 }
--- app/src/main/java/com/example/gitrobotdreamslessonssaveddata/Myapplication.kt
+++ app/src/main/java/com/example/gitrobotdreamslessonssaveddata/Myapplication.kt
@@ -6,20 +6,27 @@ import android.content.SharedPreferences
 
 class Myapplication: Application() {
     private lateinit var mySharedPrefs:SharedPreferences
-
+    private var numberAsString:String = ""
     override fun onCreate() {
         super.onCreate()
         instance = this
-        mySharedPrefs = getSharedPreferences("MyNewSharedPrefs", Context.MODE_PRIVATE)
+        mySharedPrefs = getSharedPreferences(getString(R.string.name_of_shared_prefs), Context.MODE_PRIVATE)
     }
 
-    fun saveData(numberAsString:String){
+    fun saveNumber(number:Int){
+        numberAsString = number.toString()
         mySharedPrefs.edit().putString(prefsKey, numberAsString).apply()
     }
 
-    fun getSavedData(): String {
-        return mySharedPrefs.getString(prefsKey, "") ?: ""
+    fun getSavedNumber(): Int{
+        var number = 0
+        numberAsString = mySharedPrefs.getString(prefsKey, "") ?: ""
+        if(numberAsString != "") {
+            number = numberAsString.toInt()
+        }
+        return number
     }
+
     companion object{
         private const val prefsKey = "MyNumberString"
         private lateinit var instance:Myapplication
